2a991fc01c6bc17a16d17a3a50e332f8f39f4b66
Elliot <34463580+ElliotFriedman@users.noreply.github.com>
2019-01-31 20:02:43 -0800

    Added dynamic indexing

--- TCR.sol
+++ TCR.sol
@@ -4,7 +4,6 @@ contract simpleTCR {
     
     uint256 depositPrice;
     uint8 constant totalListings = 10;
-    uint8 currentListingIndex;
     mapping (uint8 => bool) indexFree;
     
     mapping (address => uint256) public tokenBalance;
@@ -24,7 +23,14 @@ contract simpleTCR {
         tokenBalance[address(0)] = 1000000000000000000;
         depositPrice = 100;
     }
-    
+
+    function sendTokens(address sendto, uint amt) public
+	{
+		require(tokenBalance[msg.sender] >= amt);
+		tokenBalance[msg.sender] -= amt;
+		tokenBalance[sendto] += amt;
+	}
+
     function transferTokens(address sendfrom, address sendto, uint amt) internal
 	{
 		require(tokenBalance[sendfrom] >= amt);
@@ -42,30 +48,53 @@ contract simpleTCR {
 
     function createListing(string memory _businessName, string memory _businessAddress, int40 _phoneNumber) public
     {
-        require(currentListingIndex < totalListings);
-        require(!indexFree[currentListingIndex]);
+        //if there isn't a free index we revert
+        uint8 index = getFreeIndex();
+        require(index < totalListings);
+        require(!indexFree[index]);
         require(tokenBalance[msg.sender] >= depositPrice);
         
-        indexFree[currentListingIndex] = true;
+        //set the current index to being used and withdraw tokens from person
+        indexFree[index] = true;
         tokenBalance[msg.sender] -= depositPrice;
         tokenDeposited[msg.sender] += depositPrice;
         
-        listed[currentListingIndex] = Listing({businessName: _businessName,
-        businessAddress: _businessAddress,
-        phoneNumber: _phoneNumber, 
-        isListed: true, 
-        businessOwner: msg.sender});
+        listed[index] = Listing({businessName: _businessName,
+           businessAddress: _businessAddress,
+            phoneNumber: _phoneNumber, 
+            isListed: true, 
+            businessOwner: msg.sender});
+    }
+    
+    function getFreeIndex() public view returns (uint8)
+    {
+        for (uint8 i = 0; i < 10; i++)
+        {
+            if (indexFree[i] == false)
+                return i;
+        }
+        //error code
+        require(false, "Error, no free slots");
+        return 255;
+    }
+    
+    function destroyListing(uint8 index) public
+    {
+        require(index < totalListings);
         
-        currentListingIndex++;
+        require(listed[index].businessOwner == msg.sender);
+        require(listed[index].isListed == true);
+
+        indexFree[index] = false;
     }
     
     function getBusiness(uint8 index) public view returns (string memory, string memory, int40, bool, address)
     {
-        return (listed[index].businessName, 
-        listed[index].businessAddress, 
-        listed[index].phoneNumber, 
-        listed[index].isListed, 
-        listed[index].businessOwner); 
+        return (listed[index].businessName,
+            listed[index].businessAddress,
+            listed[index].phoneNumber,
+            listed[index].isListed,
+            listed[index].businessOwner);
     }
  
 }
